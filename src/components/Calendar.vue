58870185a72b69efeea452e435ec8290fb076cf9
Stephen McMahon <steve.mcmahons@gmail.com>
2024-03-15 22:34:55 -0400

    calendar input form

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,20 +1,47 @@
 <script setup>
   import VueCal from 'vue-cal'
+  import { ref } from 'vue'
+
+  const events = ref([])
+  const options = {
+    editable: true,
+    draggable: true,
+    resizable: true,
+    defaultView: 'month',
+    disableViews: ['years', 'year', 'week', 'day']
+  }
+
+  const newEvent = ref({
+    title: '',
+    start: '',
+    end: ''
+  })
+
+  const addEvent = () => {
+    events.value.push({
+      title: newEvent.value.title,
+      start: new Date(newEvent.value.start),
+      end: new Date(newEvent.value.end)
+    })
+
+    // Clear the form fields
+    newEvent.value = { title: '', start: '', end: '' }
+  }
 </script>
 
 <template>
-  <vue-cal
-    hide-view-selector
-    :time="false"
-    active-view="month"
-    xsmall>
-    <template #arrow-prev>
-      <i class="icon material-icons">arrow_back</i>
-    </template>
-    <template #arrow-next>
-      <i class="icon material-icons">arrow_forward</i>
-    </template>
-  </vue-cal>
+  <div>
+    <vue-cal v-model="events" :options="options"></vue-cal>
+    <form @submit.prevent="addEvent">
+      <label for="title">Title:</label>
+      <input type="text" id="title" v-model="newEvent.title" required><br>
+      <label for="start">Start Date:</label>
+      <input type="datetime-local" id="start" v-model="newEvent.start" required><br>
+      <label for="end">End Date:</label>
+      <input type="datetime-local" id="end" v-model="newEvent.end" required><br>
+      <button type="submit">Add Event</button>
+    </form>
+  </div>
 </template>
 
 <style scoped lang="scss">
